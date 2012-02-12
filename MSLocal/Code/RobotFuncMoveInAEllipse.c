ecce32676dd06144e0849754da386c8577039cb3
prashantmahajan.ma@gmail.com <prashantmahajan.ma@gmail.com@14c15e8c-40f0-720d-9d1a-d229be98e001>
2012-02-12 00:57:38 +0000

    commit the last updated file.

--- MSLocal/Code/RobotFuncMoveInAEllipse.c
+++ MSLocal/Code/RobotFuncMoveInAEllipse.c
@@ -1,8 +1,31 @@
 /*
  * RobotFuncMoveInAEllipse.c
  *
- *  Created on: Nov 24, 2011
+ *  Created on: Dec 18, 2011
  *      Author: Prashant
  */
 
+#include <machinescience.h>
+#include <mastermod.h>
+#include <mxapi.h>
+#include "RobotFuncMoveInAEllipse.h"
 
+void _goStraight () {
+	int v_iTime = 0;
+	servo_motor(0, +100);
+	servo_motor(1, -100);
+	v_iTime = _goBackInfoInstance->distance * 100;
+	delay_ms (v_iTime);
+	servo_robot(STOP, 0);
+}
+
+void _setGoStraightDistance (int prm_iDistance) {
+	_goStraightInfoInstance->distance = prm_iDistance;
+}
+
+struct RobotFuncGoStraight goStraight_GetInstance () {
+	struct RobotFuncGoStraight v_Return;
+	v_Return.setDistance =_setGoStraightDistance;
+	v_Return.execute = _goStraight;
+	return v_Return;
+}
